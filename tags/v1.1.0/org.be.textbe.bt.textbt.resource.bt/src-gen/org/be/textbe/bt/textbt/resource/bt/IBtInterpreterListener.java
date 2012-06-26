b4376f44c7643f119dcb06954ab18a7248bcee49
jgsuess@gmail.com <jgsuess@gmail.com@f0832461-7c65-4917-3b45-1a57d2e10441>
2012-06-26 13:15:34 +0000

    Formal maintenance release

---
+++ tags/v1.1.0/org.be.textbe.bt.textbt.resource.bt/src-gen/org/be/textbe/bt/textbt/resource/bt/IBtInterpreterListener.java
@@ -0,0 +1,12 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * 
+ */
+package org.be.textbe.bt.textbt.resource.bt;
+
+public interface IBtInterpreterListener {
+	
+	public void handleInterpreteObject(org.eclipse.emf.ecore.EObject element);
+}
---
+++ tags/v1.1.0/org.be.textbe.bt.textbt.resource.bt/src-gen/org/be/textbe/bt/textbt/resource/bt/mopp/BtFoldingInformationProvider.java
@@ -0,0 +1,16 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * 
+ */
+package org.be.textbe.bt.textbt.resource.bt.mopp;
+
+public class BtFoldingInformationProvider {
+	
+	public org.eclipse.emf.ecore.EClass[] getFoldableClasses() {
+		return new org.eclipse.emf.ecore.EClass[] {
+		};
+	}
+	
+}
