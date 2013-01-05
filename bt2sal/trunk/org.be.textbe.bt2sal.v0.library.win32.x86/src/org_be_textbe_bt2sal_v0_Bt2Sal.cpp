51426ecb5eeb9fb2d8f861bb364277c1eb65341d
jgsuess@gmail.com <jgsuess@gmail.com@f0832461-7c65-4917-3b45-1a57d2e10441>
2013-01-05 05:35:46 +0000

    Added updated JNI dispatch

--- bt2sal/trunk/org.be.textbe.bt2sal.v0.library.win32.x86/src/org_be_textbe_bt2sal_v0_Bt2Sal.cpp
+++ bt2sal/trunk/org.be.textbe.bt2sal.v0.library.win32.x86/src/org_be_textbe_bt2sal_v0_Bt2Sal.cpp
@@ -2,27 +2,32 @@
 #include "TranslateSALMain.h"
 #include "NString.h"
 
-JNIEXPORT jstring JNICALL Java_org_be_textbe_bt2sal_v0_Bt2Sal_translateToSal(JNIEnv* env, jobject jobj, jstring strTree, jint iPriority, jint iBuff, jint iWithSets, jstring strSets, jstring strInit){
-	CTranslateSALMain cTranslator; 
+JNIEXPORT jstring JNICALL Java_org_be_textbe_bt2sal_v0_Bt2Sal_translateToSal(
+		JNIEnv* env, jobject jobj, jstring strTree, jint iPriority, jint iBuff,
+		jint iWithSets, jstring strSets, jstring strInit) {
+	CTranslateSALMain cTranslator;
 	// Convert the jstrings to char strings.
-	//const char* strConvertedTree = (const char*) strTree;
-	//const jchar* strJTree = env->GetStringUTFChars(strTree, NULL); 
-	const char* strConvertedTree = env->GetStringUTFChars(strTree, NULL); //(const char*) strJTree;
-//	const jchar* strJSets = env->GetStringUTFChars(strSets, NULL); 
-	const char* strConvertedSets = env->GetStringUTFChars(strSets, NULL);// = (const char*) strJSets;
-//	const jchar* strJInit = env->GetStringUTFChars(strInit, NULL); 
-	const char* strConvertedInit = env->GetStringUTFChars(strInit, NULL);// = (const char*) strJInit;
-	
+
+	const char* strConvertedTree = env->GetStringUTFChars(strTree, 0);
+	const char* strConvertedSets = env->GetStringUTFChars(strSets, 0);
+	const char* strConvertedInit = env->GetStringUTFChars(strInit, 0);
+
 	// Call the translator.
-	cTranslator.SelectOptions(strConvertedTree, iPriority, iBuff, iWithSets, strConvertedSets, strConvertedInit);
+	cTranslator.SelectOptions(strConvertedTree, iPriority, iBuff, iWithSets,
+			strConvertedSets, strConvertedInit);
 	NString strNResult = cTranslator.ParseBT(0, false, false);
-	
-	// Convert the result into a jstring.
-	static std::string strWResult = strNResult.GetString();
+
+	env->ReleaseStringUTFChars(strInit, strConvertedInit);
+	env->ReleaseStringUTFChars(strSets, strConvertedSets);
+	env->ReleaseStringUTFChars(strTree, strConvertedTree);
+
+	/*
+	 * Convert the result into a jstring.
+	 */
+	std::string strWResult = strNResult.GetString();
 	const char* strFinalResult = strWResult.c_str();
-//	const jchar* strJResult = (const jchar*) strFinalResult;
-//	unsigned int iLength = wcslen(strFinalResult);
-   return env->NewStringUTF(strFinalResult);
-//	return strTree;
+
+	return env->NewStringUTF(strFinalResult);
+
 }
 
