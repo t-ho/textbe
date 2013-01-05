67f935deae000fe4d59a20538627feaecf99e79d
jgsuess@gmail.com <jgsuess@gmail.com@f0832461-7c65-4917-3b45-1a57d2e10441>
2013-01-05 06:09:52 +0000

    Branch 1.0.0

---
+++ bt2sal/branches/1.0.0/org.be.textbe.bt2sal.v0.test/src/org/be/textbe/bt2sal/test/Bt2Sal.java
@@ -0,0 +1,19 @@
+package org.be.textbe.bt2sal.test;
+
+class Bt2Sal {
+	private static native String translateToSal(String tree, int priority, int buff,
+			int withSets, String sets, String init);
+
+	public static void main(String[] args) {
+		System.loadLibrary("bt2sal");
+		String tree = "#RT R1 R1\r\n#C C1 DOOR\r\n#S 1 Closed\r\n#C C2 USER\r\n#S 1 sdfs\r\n#T R1 C1 1; R1 C2 1\r\n\r\n";
+		int priority = 0;
+		int buff = 0;
+		int withSets = 0;
+		String sets = "";
+		String init = "";
+		String result = Bt2Sal.translateToSal(tree, priority, buff, withSets,
+				sets, init);
+		System.out.println(result);
+	}
+}
